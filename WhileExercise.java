79e82fd3fda9d581edc6385f3250eac5999bb299
A1an1u <1390703178@qq.com>
2024-08-03 17:56:42 +0800

    2024-08-03  A1an1u

--- WhileExercise.java
+++ WhileExercise.java
@@ -5,5 +5,13 @@ public class WhileExercise {
             System.out.println(i + "+" + (j-i) + "=" + j);
             i++;
         }
+        i = 20;
+        //打印20~200之间的偶数
+        while(i>=20&&i<=200){
+            if(i%2==0){
+                System.out.println(i);
+            }
+            i++;
+        }
     }
 }
